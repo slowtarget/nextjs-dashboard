a68b2f846d869cc43c4101dd75b5abad710b8474
Jason Farmer <jasonf@motabilityoperations.co.uk>
2024-02-03 23:20:29 +0000

    chapter 11 completed

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,12 +4,16 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search({
+  placeholder,
+}: Readonly<{ placeholder: string }>) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
     if (term) {
       params.set('query', term);
     } else {
