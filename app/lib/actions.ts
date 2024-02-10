56ab71a53094c92dab37751d5ebec28a41c08bb3
Jason Farmer <jasonf@motabilityoperations.co.uk>
2024-02-10 22:35:59 +0000

    chapter 13 - error handling - complete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -67,10 +67,11 @@ export async function updateInvoice(id: string, formData: FormData) {
 export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
   } catch (e) {
     return {
       message: 'Database Error: Failed to Delete Invoice.',
     };
   }
-  revalidatePath('/dashboard/invoices');
 }
